fad50bfc40732586170dc8e1107e30de1845a6c5
Wahyu <wahyu.kodar@pintap.id>
2023-04-04 13:44:02 +0700

    define get and post method

--- src/main/java/com/sawitpro/OCRServlet.java
+++ src/main/java/com/sawitpro/OCRServlet.java
@@ -1,7 +1,36 @@
 package com.sawitpro;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class OCRServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("Only the post method is available");
+    }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String text = "test";
+        response.setContentType("text/plain");
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        try {
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.setStatus(500);
+            response.getWriter().write(e.getMessage());
+            return;
+        }
+        response.setStatus(200);
+        response.getWriter().write(text);
+    }
 }
